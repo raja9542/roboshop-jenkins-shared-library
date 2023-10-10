abf42fdd0d2810811af5dd817465fba8a0a501a5
raja9542 <rajasekhar1banda@gmail.com>
2023-10-10 11:33:51 +0530

    jenkins shared library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -10,7 +10,9 @@ def call() {
 
             stage('Compile/Build') {
                 steps{
-                    echo 'compile'
+                    script{
+                        common.compile()
+                    }
                 }
             }
 
@@ -35,3 +37,48 @@ def call() {
         }
     }
 }
+
+
+
+
+
+
+
+
+//def call() {
+//
+//    pipeline{
+//
+//        agent{
+//            label 'workstation'
+//        }
+//
+//        stages{
+//
+//            stage('Compile/Build') {
+//                steps{
+//                    echo 'compile'
+//                }
+//            }
+//
+//            stage('Unit Tests') {
+//                steps{
+//                    echo 'Unit Tests'
+//                }
+//            }
+//
+//            stage('Quality Control') {
+//                steps{
+//                    echo 'Quality Control'
+//                }
+//            }
+//
+//            stage('Upload code to a centralized place') {
+//                steps{
+//                    echo 'upload'
+//                }
+//            }
+//
+//        }
+//    }
+//}
