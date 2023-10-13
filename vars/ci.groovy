c3df3ec43896a8de5c945dc70d89ed25344e9f79
raja9542 <rajasekhar1banda@gmail.com>
2023-10-13 12:41:23 +0530

    compile

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -32,7 +32,7 @@ def call() {
                     }
                     steps {
 
-                      sh "sonar-scanner -Dsonar.host.url=http://172.31.45.15:9000 -Dsonar.login="${SONAR_USER}"  -Dsonar.password="${SONAR_PASS}" -Dsonar.projectKey=cart"
+                      sh "sonar-scanner -Dsonar.host.url=http://172.31.45.15:9000 -Dsonar.login=${SONAR_USER}  -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
                     }
                 }
 
