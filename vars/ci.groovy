53e5b1bc5989f200b930eb0bd5be13f962b2c7d0
raja9542 <rajasekhar1banda@gmail.com>
2023-10-16 11:51:02 +0530

    compile

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -25,35 +25,34 @@ def call() {
                 }
 
                 stage('Quality Control') {
-
                     environment {
-                        SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                       // SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        //SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
                     }
                     steps {
                         script {
-                            SONAR_PASS = sh (script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                            SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
                             wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-                                println "Password: ${SONAR_PASS}"
-                                sh "sonar-scanner -Dsonar.host.url=http://172.31.45.15:9000 -Dsonar.login=${SONAR_USER}  -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+                                sh "sonar-scanner -Dsonar.host.url=http://172.31.11.33:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
                             }
                         }
                     }
                 }
 
-                stage('Upload code to a centralized place') {
+                stage('Upload Code to Centralized Place') {
                     steps {
-                        echo 'upload'
+                        echo 'Upload'
                     }
                 }
 
+
             }
-            }
-        } catch (Exception e) {
+
+        }
+    } catch(Exception e) {
         common.email("Failed")
     }
 }
-
 
 
 
