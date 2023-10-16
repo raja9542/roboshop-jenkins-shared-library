cd9f5fcb62eac6c91eeb49e4d46236b7ca50b1dd
raja9542 <rajasekhar1banda@gmail.com>
2023-10-16 12:26:08 +0530

    compile

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -33,7 +33,7 @@ def call() {
                         script {
                             SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
                             wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-                                sh "sonar-scanner -Dsonar.host.url=http://172.31.45.15:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+                                sh "sonar-scanner -Dsonar.host.url=http://172.31.45.15:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey={component}"
                             }
                         }
                     }
