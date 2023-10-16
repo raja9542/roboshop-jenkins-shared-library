0b952fc28840a612ef489538da3d723bf9df7161
raja9542 <rajasekhar1banda@gmail.com>
2023-10-16 12:10:14 +0530

    compile

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -27,7 +27,7 @@ def call() {
                 stage('Quality Control') {
                     environment {
                         SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                        //SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
                     }
                     steps {
                         sh "sonar-scanner -Dsonar.host.url=http://172.31.11.33:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component}"
