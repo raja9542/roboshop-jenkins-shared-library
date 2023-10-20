3eb7927e7b904a52c3193914b2550ea9995cc886
raja9542 <rajasekhar1banda@gmail.com>
2023-10-20 11:52:46 +0530

    jenkins env compare

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -4,6 +4,12 @@ def call() {
         env.SONAR_EXTRA_OPTS=" "
     }
 
+    if(!env.TAG_NAME){
+        env.PUSH_CODE="false"
+    } else {
+        env.PUSH_CODE="true"
+    }
+
     try {
         node('workstation') {
 
@@ -29,10 +35,12 @@ def call() {
                 }
             }
 
-            stage('Upload Code to Centralized Place') {
-                echo 'Upload'
-            }
+            if(env.PUSH_CODE == true) {
 
+                stage('Upload Code to Centralized Place') {
+                    echo 'Upload'
+                }
+            }
 
         }
 
