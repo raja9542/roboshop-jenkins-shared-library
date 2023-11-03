16b68e981e934d3dfe09bcc96a31da5365c63c16
raja9542 <rajasekhar1banda@gmail.com>
2023-11-03 19:34:56 +0530

    nexus

--- vars/common.groovy
+++ vars/common.groovy
@@ -39,7 +39,7 @@ def email(email_note) {
 def artifactPush() {
     if (app_lang == "nodejs") {
 
-        zip -r cart-${TAG_NAME}.zip node_modules server.js
+        sh "zip -r cart-${TAG_NAME}.zip node_modules server.js"
     }
     sh "ls -l"
 }
