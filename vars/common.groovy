ace93fd3d3597e6fd7aefd6448e5fafc5f46acbe
raja9542 <rajasekhar1banda@gmail.com>
2023-11-03 21:10:49 +0530

    nexus

--- vars/common.groovy
+++ vars/common.groovy
@@ -46,7 +46,7 @@ def artifactPush() {
     NEXUS_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
     NEXUS_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
     wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASS}", var: 'SECRET']]]) {
-        sh "curl -v -u {NEXUS_USER}:{NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.40.116:8081/repository/${component}/${component}-${TAG_NAME}.zip"
+        sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.40.116:8081/repository/${component}/${component}-${TAG_NAME}.zip"
     }
 
 }
