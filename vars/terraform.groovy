af4d788a7db388c4a01d0ed238ea29f68bece2a7
raja9542 <rajasekhar1banda@gmail.com>
2023-11-08 20:15:46 +0530

    vpc module

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -20,12 +20,13 @@ def call() {
                     sh "terraform init -backend-config=env-${INFRA_ENV}/state.tfvars"
                 }
             }
-        }
+
             stage('Terraform Apply') {
                 steps{
                     sh "terraform apply -auto-approve -var-file=env-${INFRA_ENV}/main.tfvars"
                 }
             }
+        }
 
         post {
             always {
