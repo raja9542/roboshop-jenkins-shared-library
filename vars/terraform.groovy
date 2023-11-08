e446de475ba6cc6d7f875ac568d79db0cca7dcac
raja9542 <rajasekhar1banda@gmail.com>
2023-11-08 10:24:30 +0530

    terraform proj

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -10,7 +10,7 @@ def call() {
             string(name: 'INFRA_ENV', defaultValue: '', description: 'Enter env like dev or prod')
         }
 
-        satges{
+        stages{
             stage('Terraform Init') {
                 sh "terraform init -backend-config=env-${INFRA_ENV}/state.tfvars"
             }
