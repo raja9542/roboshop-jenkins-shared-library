2503f2c3232ca4765f56e7a1b0068f4e2c7d094f
raja9542 <rajasekhar1banda@gmail.com>
2023-11-22 15:25:40 +0530

    deploy groovy updated

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -10,13 +10,26 @@ def call() {
             string(name: 'APP_VERSION', defaultValue: '', description: 'Enter application version to deploy')
         }
 
+        options {
+            ansiColor('xterm')
+        }
+
+        environment {
+
+            SSH=credentials('SSH')
+        }
+
         stages{
 
             stage('Run Deployment'){
 
                 steps{
 // to update/overwrite app version in parameter store we use this
-                    sh '''aws ssm put-parameter --name "${APP_ENV}.${COMPONENT}.APP_VERSION" --type "String" --value "${APP_VERSION}" --overwrite '''
+                    sh '''
+                      aws ssm put-parameter --name "${APP_ENV}.${COMPONENT}.APP_VERSION" --type "String" --value "${APP_VERSION}" --overwrite 
+                      aws ec2 describe-instances --filters "Name=tag:Name,Values=${APP_ENV}-${COMPONENT}" | jq ".Reservations[].Instances[].PrivateIpAddress\" >/tmp/hosts
+                      ansible-playbook -i /tmp/hosts deploy.yml -e component=${COMPONENT} -e env=${APP_ENV} -e ansible_user=${SSH_USR) -e ansible_password=${SSH_PSW}
+                      '''
                 }
 
             }
@@ -24,3 +37,5 @@ def call() {
 
     }
 }
+
+// aws ec2 describe-instances --filters "Name=tag:Name,Values=prod-shipping" | jq '.Reservations[].Instances[].PrivateIpAddress'
